4034d08eebaba657e2e7545b07ca9bd7db70b889
Nuttykomu (ナッティ) <komu@nutty.world>
2018-06-13 16:20:34 -0700

    Fix initial capture state

--- source/onitama/State.cc
+++ source/onitama/State.cc
@@ -8,16 +8,16 @@ GameState getNewGameState() {
     GameState state = {};
 
     // 1. Set up the pieces on the board.
-    state.pieces |= 0b00010'1LL << (0 * 6);  // Position 02, Not Captured [BLUE MASTER]
-    state.pieces |= 0b00000'1LL << (1 * 6);  // Position 00, Not Captured [BLUE PAWN]
-    state.pieces |= 0b00001'1LL << (2 * 6);  // Position 01, Not Captured [BLUE PAWN]
-    state.pieces |= 0b00011'1LL << (3 * 6);  // Position 03, Not Captured [BLUE PAWN]
-    state.pieces |= 0b00100'1LL << (4 * 6);  // Position 04, Not Captured [BLUE PAWN]
-    state.pieces |= 0b10110'1LL << (5 * 6);  // Position 22, Not Captured [RED MASTER]
-    state.pieces |= 0b10100'1LL << (6 * 6);  // Position 20, Not Captured [RED PAWN]
-    state.pieces |= 0b10101'1LL << (7 * 6);  // Position 21, Not Captured [RED PAWN]
-    state.pieces |= 0b10111'1LL << (8 * 6);  // Position 23, Not Captured [RED PAWN]
-    state.pieces |= 0b11000'1LL << (9 * 6);  // Position 24, Not Captured [RED PAWN]
+    state.pieces |= 0b00010'0LL << (0 * 6);  // Position 02, Not Captured [BLUE MASTER]
+    state.pieces |= 0b00000'0LL << (1 * 6);  // Position 00, Not Captured [BLUE PAWN]
+    state.pieces |= 0b00001'0LL << (2 * 6);  // Position 01, Not Captured [BLUE PAWN]
+    state.pieces |= 0b00011'0LL << (3 * 6);  // Position 03, Not Captured [BLUE PAWN]
+    state.pieces |= 0b00100'0LL << (4 * 6);  // Position 04, Not Captured [BLUE PAWN]
+    state.pieces |= 0b10110'0LL << (5 * 6);  // Position 22, Not Captured [RED MASTER]
+    state.pieces |= 0b10100'0LL << (6 * 6);  // Position 20, Not Captured [RED PAWN]
+    state.pieces |= 0b10101'0LL << (7 * 6);  // Position 21, Not Captured [RED PAWN]
+    state.pieces |= 0b10111'0LL << (8 * 6);  // Position 23, Not Captured [RED PAWN]
+    state.pieces |= 0b11000'0LL << (9 * 6);  // Position 24, Not Captured [RED PAWN]
 
     // 2. Draw five move cards randomly.
     std::default_random_engine rng((unsigned int)time(0));
@@ -29,7 +29,7 @@ GameState getNewGameState() {
 
     // 3. Determine who goes first.
     const int EXTRA_CARD_OFFSET = 16;
-    int extraCardMask = (0b1111 << EXTRA_CARD_OFFSET);
+    int extraCardMask = 0b1111 << EXTRA_CARD_OFFSET;
     int extraCardIndex = (state.cards & extraCardMask) >> EXTRA_CARD_OFFSET;
     state.turn = CardList[extraCardIndex].color;
 
